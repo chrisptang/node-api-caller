19c7c269bba5b16bcae0b45c60d57a77fbb0c7a3
tang peng <tangpeng@tangs-MacBook-Pro.local>
2024-03-08 12:17:03 +0800

    appending new notes to Joplin so that i can keep all dialogues as notes

--- anthropic-api-it.mjs
+++ anthropic-api-it.mjs
@@ -2,6 +2,7 @@ import Anthropic from '@anthropic-ai/sdk';
 import { HttpsProxyAgent } from "https-proxy-agent";
 import fs from 'fs'
 import moment from 'moment'
+import fetch from 'node-fetch';
 import os from 'os'
 import path from 'path'
 import readline from 'readline'
@@ -10,17 +11,73 @@ const MODEL = process.env.CLAUDE_MODEL || 'claude-3-opus-20240229'
 const MD_LINE_BREAK = '\n\n'
 const HUMAN_COLOR = '<p style="border-radius: 5px;padding:2px;background-color: lightskyblue;color:#fff;font-size:larger;font-weight:bolder;">', HUMAN_COLOR_END = '</p>'
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-  terminal: false
-});
-
 const TIME_OF_NOW = new Date().toISOString().split("T")[0]
 
 const homeDir = os.homedir();
 const dirToCreate = path.join(homeDir, 'claude');
 const now = moment().format('YYYY-MM-DD HH:mm:ss');
+const today = moment().format('YYYYMMDD');
+
+function generateRandomString(size = 32) {
+  const characters = 'abcdef0123456789';
+  let randomString = '';
+  size = Math.max(10, size - today.length)
+
+  // Generate random characters
+  for (let i = 0; i < size; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    randomString += characters.charAt(randomIndex);
+  }
+
+  // Combine datetime and random string
+  const result = `${today}${randomString}`;
+  return result;
+}
+
+const NEW_NOTE_ID = generateRandomString();
+console.log(`NEW_NOTE_ID:${NEW_NOTE_ID}`)
+
+const JOPLIN_NOTE_SERVER = 'http://127.0.0.1:41184';
+const JOPLIN_TOKEN = process.env.JOPLIN_TOKEN || '';
+
+let NEED_CREATE_NOTE = true
+
+async function createNewNoteWithIdAndBody(body, title) {
+  title = `${today}: ${title}`
+  title = title.substring(0, Math.min(60, title.length))
+
+  const url = NEED_CREATE_NOTE ? `${JOPLIN_NOTE_SERVER}/notes` : `${JOPLIN_NOTE_SERVER}/notes/${NEW_NOTE_ID}`
+  let data = {
+    body, parent_id: '9bb587b94369470c873c5af096648b75'
+  };
+
+  if (NEED_CREATE_NOTE) {
+    data.id = NEW_NOTE_ID;
+    data.title = title;
+  }
+
+  const response = await fetch(`${url}?token=${JOPLIN_TOKEN}`, {
+    method: NEED_CREATE_NOTE ? 'POST' : 'PUT',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  });
+
+  const dataResponse = await response.json();
+  if (dataResponse.error) {
+    console.log(JSON.stringify(dataResponse))
+  } else {
+    NEED_CREATE_NOTE = false
+  }
+  return dataResponse;
+}
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  terminal: false
+});
 
 fs.mkdirSync(dirToCreate, { recursive: true }, (error) => {
   if (error) {
@@ -62,6 +119,7 @@ function append_new_message(msg, is_assistant = false) {
         }
       ]
     });
+  // console.log(JSON.stringify(SESSION_CACHE))
   return SESSION_CACHE;
 }
 
@@ -70,6 +128,8 @@ if (process.argv.length > 2) {
   let prompt = process.argv[2];
   call(prompt)
 }
+
+let session_content_full = ""
 
 // Override per-request:
 async function call(prompt) {
@@ -98,10 +158,12 @@ async function call(prompt) {
       process.stdout.write(`${new_text}`);
     }
   }
-  process.stdout.write(MD_LINE_BREAK)
+  process.stdout.write(`${MD_LINE_BREAK}>>>`)
   append_new_message(completions, true)
-  fs.appendFileSync(fileName, `${MD_LINE_BREAK}###${now} ${MD_LINE_BREAK}${HUMAN_COLOR}${Anthropic.HUMAN_PROMPT} ${prompt}${HUMAN_COLOR_END} ${Anthropic.AI_PROMPT} ${completions}${MD_LINE_BREAK}`)
-  console.log(`response was wroten into file:${fileName}`)
+  session_content_full = `${session_content_full}${MD_LINE_BREAK}### ${now} ${MD_LINE_BREAK}${HUMAN_COLOR}${prompt}${HUMAN_COLOR_END} ${Anthropic.AI_PROMPT} ${completions}${MD_LINE_BREAK}`
+  fs.appendFileSync(fileName, session_content_full)
+  await createNewNoteWithIdAndBody(session_content_full, prompt)
+  // console.log(`response was wroten into file:${fileName}`)
 }
 
 let lines = [];
@@ -114,8 +176,9 @@ rl.on('line', async (input) => {
       return;
     }
     lines.push(input);
-    if (lines.length > 0) {
-      await call(lines.join("\n"))
+    const line_content = lines.join("\n");
+    if (line_content.length > 2) {
+      await call(line_content)
       lines = [];
     }
   }
