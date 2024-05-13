71e0aec6dbba66e4e19cf337d62da8eb083313ab
tang peng <tangpeng@tangs-MacBook-Pro.local>
2024-05-13 15:57:14 +0800

    works with Llama3 at groq APIs

--- groq-api-it.mjs
+++ groq-api-it.mjs
@@ -1,4 +1,3 @@
-import Anthropic from '@anthropic-ai/sdk';
 import { HttpsProxyAgent } from "https-proxy-agent";
 import fs from 'fs'
 import moment from 'moment'
@@ -7,16 +6,36 @@ import os from 'os'
 import path from 'path'
 import readline from 'readline'
 
-const MODEL = process.env.CLAUDE_MODEL || 'claude-3-opus-20240229'
+const MODEL = process.env.GROQ_MODEL || 'llama3-70b-8192'
 const MD_LINE_BREAK = '\n\n'
 const HUMAN_COLOR = '\n> ', HUMAN_COLOR_END = '\n\n'
 
 const TIME_OF_NOW = new Date().toISOString().split("T")[0]
 
 const homeDir = os.homedir();
-const dirToCreate = path.join(homeDir, 'claude');
+const dirToCreate = path.join(homeDir, 'groq-llama3');
 const now = moment().format('YYYY-MM-DD HH:mm:ss');
 const today = moment().format('YYYYMMDD');
+
+const HTTP_PROXY = process.env.HTTP_PROXY || 'http://127.0.0.1:8001';
+const proxyAgent = new HttpsProxyAgent(HTTP_PROXY);
+console.log(`using HTTP_PROXY:${HTTP_PROXY}`)
+
+import { Groq } from 'groq-sdk';
+
+const groq = new Groq({ httpAgent: proxyAgent, });
+
+async function getGroqChatCompletion(messages = []) {
+  return groq.chat.completions.create({
+    messages: messages,
+    model: MODEL,
+    temperature: 0.9,
+    max_tokens: 8000,
+    top_p: 1,
+    stop: null,
+    stream: true
+  });
+}
 
 function generateRandomString(size = 32) {
   const characters = 'abcdef0123456789';
@@ -48,7 +67,7 @@ async function createNewNoteWithIdAndBody(body, title) {
 
   const url = NEED_CREATE_NOTE ? `${JOPLIN_NOTE_SERVER}/notes` : `${JOPLIN_NOTE_SERVER}/notes/${NEW_NOTE_ID}`
   let data = {
-    body, parent_id: '9bb587b94369470c873c5af096648b75'
+    body, parent_id: '4f9f4819a9b645f0a84c02a5dd5c32c9' // LLaMa3@Groq
   };
 
   if (NEED_CREATE_NOTE) {
@@ -93,31 +112,14 @@ if (!fs.existsSync(fileName)) {
   fs.writeFileSync(fileName, '');
 }
 
-const HTTP_PROXY = process.env.HTTP_PROXY || 'http://0.0.0.0:8001';
-
-const proxyAgent = new HttpsProxyAgent(HTTP_PROXY);
-
-console.log(`using HTTP_PROXY:${HTTP_PROXY}`)
-
-// Configure the default for all requests:
-const anthropic = new Anthropic({
-  httpAgent: proxyAgent,
-  apiKey: `${process.env.ANTHROPIC_API_KEY}`
-});
-
-
 const SESSION_CACHE = [];
 
+// https://console.groq.com/docs/text-chat
 function append_new_message(msg, is_assistant = false) {
   SESSION_CACHE.push(
     {
       "role": is_assistant ? "assistant" : "user",
-      "content": [
-        {
-          "type": "text",
-          "text": msg
-        }
-      ]
+      "content": msg
     });
   // console.log(JSON.stringify(SESSION_CACHE))
   return SESSION_CACHE;
@@ -136,31 +138,19 @@ async function call(prompt) {
   if (!prompt || prompt.length <= 2) {
     return;
   }
-  process.stdout.write(`${MD_LINE_BREAK}User:\n${prompt}`);
-  const stream = await anthropic.messages.create(
-    {
-      max_tokens: 4096,
-      temperature: 0,
-      model: MODEL,
-      stream: true,
-      messages: append_new_message(prompt),
-    }
-  );
+  const stream = await getGroqChatCompletion(append_new_message(prompt));
   let completions = "";
-  for await (const completion of stream) {
-    if ('message_start' == `${completion.type}`) {
-      process.stdout.write(`${MD_LINE_BREAK}Claude:\n`);
-      continue;
-    }
-    if ('content_block_delta' == `${completion.type}`) {
-      const new_text = `${completion.delta.text}`
-      completions = `${completions}${new_text}`;
-      process.stdout.write(`${new_text}`);
-    }
+  process.stdout.write(`${MD_LINE_BREAK}LLaMa3:\n`);
+  for await (const chunk of stream) {
+    // Print the completion returned by the LLM.
+    const new_text = chunk.choices[0]?.delta?.content || "";
+    completions = `${completions}${new_text}`;
+    process.stdout.write(`${new_text}`);
   }
+
   process.stdout.write(`${MD_LINE_BREAK}>>>`)
   append_new_message(completions, true)
-  session_content_full = `${session_content_full}${MD_LINE_BREAK}### ${now} ${MD_LINE_BREAK}${HUMAN_COLOR}${prompt}${HUMAN_COLOR_END} ${Anthropic.AI_PROMPT} ${completions}${MD_LINE_BREAK}`
+  session_content_full = `${session_content_full}${MD_LINE_BREAK}### ${now} ${MD_LINE_BREAK}${HUMAN_COLOR}${prompt}${HUMAN_COLOR_END} \n**LLaMa3**: ${completions}${MD_LINE_BREAK}`
   fs.appendFileSync(fileName, session_content_full)
   try {
     await createNewNoteWithIdAndBody(session_content_full, prompt)
